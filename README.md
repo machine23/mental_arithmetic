ff6c643e4a1c038da6de769d14767bf4a32a4cc7
Vassili Baranov <vassili.baranov@gmail.com>
2018-09-17 21:00:46 +0300

    Init Vue app

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Mental Arithmetic Trainer
+# mental_arithmetic
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <start-screen v-if="state == 'start'"/>
+    <question v-else-if="state == 'training'"/>
+    <div v-else>Error</div>
+  </div>
+</template>
+
+<script>
+import StartScreen from './components/StartScreen.vue'
+import Question from './components/Question.vue'
+
+export default {
+  name: 'app',
+  components: {
+    StartScreen,
+    Question
+  },
+  data() {
+    return {
+      state: 'start'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
